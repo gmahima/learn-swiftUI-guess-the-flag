3834bc839056a4dbfa668686e003710280cf5c3a
gmahima <mahimagangavarapu@gmail.com>
2021-06-29 20:23:18 +0530

    Initial Commit

---
+++ guess-the-flag/guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guess_the_flagApp.swift
+//  guess-the-flag
+//
+//  Created by G H Mahimaanvita on 29/06/21.
+//
+
+import SwiftUI
+
+@main
+struct guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
