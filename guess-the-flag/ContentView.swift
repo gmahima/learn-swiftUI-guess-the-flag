c2434861338bccd04a643c415826e4c081000b57
gmahima <mahimagangavarapu@gmail.com>
2021-06-29 21:30:33 +0530

    add Color view, background etc

--- guess-the-flag/ContentView.swift
+++ guess-the-flag/ContentView.swift
@@ -9,26 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack (alignment: /*@START_MENU_TOKEN@*/Alignment(horizontal: .center, vertical: .center)/*@END_MENU_TOKEN@*/, content: {
-            VStack(alignment: .center, spacing: 20, content: {
-                ForEach(0..<3, content: {
-                    num in return HStack(alignment: .center, spacing: 38){
-                        ForEach(0..<3, content: {
-                            numin in return Text("|")
-                        })
-                    }
-                })
-            })
-            VStack(alignment: .center, spacing: 20, content: {
-                ForEach(0..<3, content: {
-                    num in return HStack(alignment: .center, spacing: 20){
-                        ForEach(0..<3, content: {
-                            numin in return Text("\(num) \(numin)")
-                        })
-                    }
-                })
-            })
-            
+        ZStack(alignment: /*@START_MENU_TOKEN@*/Alignment(horizontal: .center, vertical: .center)/*@END_MENU_TOKEN@*/, content: {
+            Color(.blue).ignoresSafeArea() //under
+            Color(red: 1, green: 1, blue: 1, opacity: 0.7).frame(width: 100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100, alignment: .leading)//places the view in an invisible frame.
+            /*@START_MENU_TOKEN@*/Text("Placeholder")/*@END_MENU_TOKEN@*/.background(Color.white) //background takes a view as arg returns the modified view. you can add Text as background too.
         })
     }
 }
